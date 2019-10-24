5d87ac2ac28ad4fed094261a1a22dcc726d88722
Blake Abel <blakecabel@gmail.com>
2019-10-24 15:09:53 -0400

    Automatically linking nav bar changer now

--- android/app/src/main/java/com/wtbu_app/MainApplication.java
+++ android/app/src/main/java/com/wtbu_app/MainApplication.java
@@ -10,8 +10,6 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-import com.thebylito.navigationbarcolor.NavigationBarColorPackage;
-
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
