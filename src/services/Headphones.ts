c71b5c3870a4e516d55354f5af447436b032bff0
Blake Abel <blakecabel@gmail.com>
2019-11-07 00:51:24 -0500

    Parsing data from the site into SHow objects now

--- src/services/Headphones.ts
+++ src/services/Headphones.ts
@@ -1,24 +1,39 @@
 import axios from 'axios'
 import HTMLParser from 'fast-html-parser'
-import Show from '../modules/Show'
+import { Show, Recording } from '../modules/Show'
 
 
 const archiveURL = 'http://headphones.bu.edu/'
+
+const rowIsHeader = function(rowString : string) : boolean {
+    return isNaN(parseInt(rowString))
+}
 
 export default class RadioShows {
     showList: Show[]
 
     constructor() {
-        this.showList = [new Show('Test', 'https://google.com', 'https://google.com', 0, 0)]
-        this.fetchData()
+        //First check if it's in local data, then set to that
+
+        //If not, then fetch new data from headphones
+        this.showList = []
+
+        //If that fails, show error and set showlist to empty.
     }
 
-    fetchData(): void {
-        axios.get(archiveURL)
+    updateShows(showArray : Show[]) : void {
+        this.showList = showArray
+    }
+
+    /**
+     * Returns updated show list.
+     */
+    async fetchData() : Promise<void | Show[]> {
+        return axios.get(archiveURL)
             .then(response => {
                 const root = HTMLParser.parse(response.data)
 
-                console.log(root.firstChild.structure.substring(0, 100))
+                //console.log(root.firstChild.structure.substring(0, 100))
                 const showTable = root.querySelector('table')
                 //console.log(showTable)
                 //console.log(showTable.text)
@@ -26,14 +41,72 @@ export default class RadioShows {
                 let showRows = showTable.querySelectorAll('tr')
                 //console.log(showRows)
 
+                //console.log("\n\n\n\n\n\n")
+                //console.log(new Date("2019-02-03T00:00"))
+
+                let almostShows = []
+
                 for (let i = 0; i < showRows.length; i++){
-                    console.log(showRows[i].text)
+                    if(!rowIsHeader(showRows[i].text)) { //we want to skip the date rows.
+
+                        //console.log(`${currentDayHeader.day} for item ${i}`)
+                        //console.log(showRows[i].querySelector('a'))
+                        //console.log(showRows[i].querySelector('a').rawAttributes)
+                        let fakeshow = {
+                            'name': showRows[i].querySelector('a').text,
+                            'broadcasts': [new Recording(showRows[i].querySelector('a').rawAttributes.href)]
+                        }
+                        //console.log(fakeshow)
+                        almostShows.push(fakeshow)
+                    }
+                    
                 }
 
-                //console.log(response.data)
-                                
-                // handle success
-                //console.log(response);
+                almostShows = almostShows.sort( (a,b) => {
+                    //console.log(`${a.name}     ${b.name}`)
+                    if(a.name.toLowerCase() === b.name.toLowerCase()){
+                        return 0
+                    } else if (a.name.toLowerCase() < b.name.toLowerCase()) {
+                        return -1
+                    } else {
+                        return 1
+                    }
+                })
+
+                //console.log(almostShows)
+
+                //merge broadcasts into single show list that has every airing of the show.
+                for(let i = 0; i < almostShows.length -1; i++){
+                    //It's the same show!
+                    //console.log(almostShows[i])
+                    //console.log(almostShows[i + 1])
+                    if(almostShows[i + 1]){
+                        if(almostShows[i].name === almostShows[i + 1].name){
+                            //console.log(`${i}, ${almostShows[i].name}`)
+                            almostShows[i].broadcasts.push(almostShows[i + 1].broadcasts[0])
+                            almostShows.splice(i + 1, 1)
+                            i-- //we must decrement after removing the dupe to handle more than two airings.
+                        } 
+                    }
+                    
+                }
+
+                let realShowList = almostShows.map( fakeshow => new Show(fakeshow.name, fakeshow.broadcasts) )
+
+                //console.log(realShowList)
+
+                realShowList.forEach( show => show.broadcasts = show.broadcasts.sort( (a,b) => {
+                    if(a.startDate.getTime() === b.startDate.getTime()){
+                        return 0
+                    } else if (a.startDate < b.startDate) {
+                        return -1
+                    } else {
+                        return 1
+                    }
+                }))
+
+                return realShowList
+
             })
             .catch(error => {
                 // handle error
@@ -46,66 +119,3 @@ export default class RadioShows {
     }
 
 }
-
-/*
-        try {
-            //Get its data
-            Elements mElementDataSize = mShowList.select("table[id=bydate]");
-            // Locate the content attribute
-            int mElementSize = mElementDataSize.size();
-
-            Log.d("WTBU-A", "tables "+ mElementSize);
-
-            Elements headers = mElementDataSize.select("strong");
-
-            List<String> linksDump = mElementDataSize.select("a").eachAttr("href");
-            ArrayList<String> links = new ArrayList<>(linksDump.size());
-            links.addAll(linksDump);
-
-            //Example LoadArchive.php?mp3file=WTBU-2019-02-03_0000_to_0200_Aircheck.mp3&realname=Aircheck
-            for(String link : links){
-                //figure out what day of the week it belongs to
-                String day = link.substring(37,39);
-                String month = link.substring(34, 36);
-                String year = link.substring(29, 33);
-
-                int weekday = dayOfWeek(Integer.parseInt(day), Integer.parseInt(month), Integer.parseInt(year)); //sunday 0 through 6 saturday
-
-                String nameHTML = link.substring(link.indexOf("=", 25) + 1);
-                String url =  "http://headphones.bu.edu/Archives/" + link.substring(link.indexOf("=") +1, link.indexOf("&"));
-
-                Show radioShow = new Show(URLDecoder.decode(nameHTML, "UTF-8"), url, "");
-
-                Log.d("WTBU-A", "" + weekday + " " + radioShow.getName());
-
-                archiveData.get(weekday).add(radioShow);
-
-            }
-
-            for(LinkedList<Show> weekday : archiveData){
-                sortShows(weekday);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return archiveData;
-
-    }
-
-    private void sortShows(LinkedList<Show> shows){
-        //sorts
-        Collections.sort(shows);
-
-        //combines duplicates
-        for(int i = 1; i < shows.size(); i++){
-            if(shows.get(i -1).isSameShow(shows.get(i))){
-                shows.get(i - 1).setUrl2(shows.get(i).getUrl1());
-                shows.remove(i);
-                i--;
-            }
-        }
-    }
-
-}*/
